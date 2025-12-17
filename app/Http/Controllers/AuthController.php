fc6965619b341101d214ade35c08120083f9e76e
adnan-taha <adnantaha123@gmail.com>
2025-12-17 16:39:36 +0300

    undo temp + fix 500 deploy

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -63,6 +63,9 @@ class AuthController extends Controller
             'password'   => Hash::make($data['password']),
             'role'       => $data['role'],   // tenant / owner
             'birth_date' => $data['birth_date'] ?? null,
+            'photo'      => $data['photo'] ?? '',
+            'id_photo_front' => $data['id_photo_front'] ?? '',
+            'id_photo_back' => $data['id_photo_back'] ?? '',
             'is_approved'=> false,           // admin must approve
         ]);
 
--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,7 @@ Route::get('/health', function () {
 });
 
 //*******AUTH*******
-//Route::middleware(['auth:sanctum', 'role:admin'])->post('/register/admin', [AuthController::class, 'register_admin']);
-Route::post('/register/admin', [AuthController::class, 'register_admin']);
+Route::middleware(['auth:sanctum', 'role:admin'])->post('/register/admin', [AuthController::class, 'register_admin']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
