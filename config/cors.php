9b5c3236f8023631e7d7ffd3b2aa8c4764783dea
adnan-taha <adnantaha123@gmail.com>
2026-01-16 23:41:59 +0300

    hot fix for 0

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,18 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], // For Flutter Web, allow all origins in development
+    'allowed_origins' => [
+        'http://localhost',
+        'http://localhost:8080',
+        'http://localhost:3000',
+        'http://localhost:5000',
+        'http://localhost:60610',
+        'http://127.0.0.1',
+        'http://127.0.0.1:8080',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:5000',
+        'http://127.0.0.1:8000',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -15,5 +26,5 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false, // Set to false when using wildcard origins with bearer tokens
+    'supports_credentials' => true, // Can be true when specifying exact origins
 ];
